948274302d2a16509f8dff441fbe62d2e1c8ae80
Vitalii Mieliet <vitalii.mieliet@gmail.com>
2025-06-11 22:42:46 +0300

    feat: add fetchMoviesByQuery API functions with pagination support

--- src/services/tmdbAPI.js
+++ src/services/tmdbAPI.js
@@ -1,14 +1,26 @@
 import axios from "axios";
 
 const tmdbAPI = axios.create({
-  baseURL: "https://api.themoviedb.org/3/movie/popular",
+  baseURL: "https://api.themoviedb.org/3",
   headers: {
     Authorization:
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMGE2NzllNTNjZjM3ZjY4YWJmYjc0NzBlYTIzYmM5NiIsIm5iZiI6MTc0OTU4NzE4My40NTIsInN1YiI6IjY4NDg5NGVmNTExNjEzNzI1MzNmZDAzZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6sgtPtYpuSZ7t0TOyNV4b8PxrJnpSBVNk2GU9Oq9f6U",
   },
 });
 
-export const fetchPopularMovies = async () => {
-  const res = await tmdbAPI.get("");
+export const fetchPopularMovies = async (page = 1) => {
+  const params = {
+    page,
+  };
+  const res = await tmdbAPI.get("/movie/popular", { params });
+  return res.data;
+};
+
+export const fetchMoviesByQuery = async (query = "", page = 1) => {
+  const params = {
+    query,
+    page,
+  };
+  const res = await tmdbAPI.get("/search/movie", { params });
   return res.data;
 };
