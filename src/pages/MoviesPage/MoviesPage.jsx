ab505499974e42b18bcd0c70ee77156f4aa2b667
Vitalii Mieliet <vitalii.mieliet@gmail.com>
2025-06-11 22:43:42 +0300

    feat: implement movie search with debounced query and URL params on MoviesPage

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,12 +1,44 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { useDebounce } from "use-debounce";
+import { fetchMoviesByQuery } from "../../services/tmdbAPI";
 import Container from "../../components/Container/Container";
 import MovieList from "../../components/MovieList/MovieList";
 import Section from "../../components/Section/Section";
 
 const MoviesPage = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get("query") ?? "";
+  const [debouncedQuery] = useDebounce(query, 300);
+
+  const changeSearchParams = (e) => {
+    const newQuery = e.target.value;
+    const newSearchParams = new URLSearchParams();
+
+    if (newQuery !== "") {
+      newSearchParams.set("query", newQuery);
+    } else {
+      newSearchParams.delete("query");
+    }
+    setSearchParams(newSearchParams);
+  };
+
+  useEffect(() => {
+    // if (!query) return;
+    const getMoviesByWuery = async () => {
+      const data = await fetchMoviesByQuery(debouncedQuery);
+      setMovies(data.results);
+    };
+    getMoviesByWuery();
+  }, [debouncedQuery]);
+
   return (
     <Section>
       <Container>
-        <MovieList />
+        <input type="text" value={query} onChange={changeSearchParams} />
+
+        <MovieList movieList={movies} />
       </Container>
     </Section>
   );
