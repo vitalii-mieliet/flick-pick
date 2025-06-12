aa5027a5a36a2993b85d921548f0d60e8a4a8bbf
Vitalii Mieliet <vitalii.mieliet@gmail.com>
2025-06-12 08:52:58 +0300

    feat: add styled NotFoundPage with fallback message and link to Home

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+import css from "./NotFoundPage.module.css";
+
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <div className={css.wrapper}>
+      <h1 className={css.title}>404 - Page Not Found</h1>
+      <p className={css.text}>
+        Sorry, the page you're looking for doesn't exist.
+      </p>
+      <Link to="/" className={css.link}>
+        Go to Home
+      </Link>
+    </div>
+  );
 };
 
 export default NotFoundPage;
