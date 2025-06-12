226d529a5deac22bfe6885d010f26d77f9587140
Vitalii Mieliet <vitalii.mieliet@gmail.com>
2025-06-12 06:36:30 +0300

    refactor: clean up and improve HomePage module

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,19 +6,31 @@ import MovieList from "../../components/MovieList/MovieList";
 
 const HomePage = () => {
   const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const getPopularMovies = async () => {
-      const data = await fetchPopularMovies();
-      setMovies(data.results);
+      try {
+        setIsLoading(true);
+        const { results } = await fetchPopularMovies();
+        setMovies(results);
+      } catch (error) {
+        setError(error.message || "Something went wrong");
+      } finally {
+        setIsLoading(false);
+      }
     };
+
     getPopularMovies();
   }, []);
 
   return (
     <Section>
       <Container>
-        <MovieList movieList={movies} />
+        {isLoading && <p>Loading...</p>}
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        {!isLoading && !error && <MovieList movieList={movies} />}
       </Container>
     </Section>
   );
