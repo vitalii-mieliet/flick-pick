d26af93a73bee949e1603d886537906566a86898
Vitalii Mieliet <vitalii.mieliet@gmail.com>
2025-06-12 07:53:54 +0300

    feat: add searchBar component

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,44 @@
+import { IoSearchSharp } from "react-icons/io5";
+import { toast, ToastContainer } from "react-toastify";
+import css from "./SearchBar.module.css";
+
+const SearchBar = ({ onSubmit }) => {
+  const handleSubmit = (formData) => {
+    const value = Object.fromEntries(formData).searchField;
+    if (!value.trim())
+      return toast.error("Oops! Looks like you forgot to type something.");
+    onSubmit(value);
+  };
+
+  return (
+    <>
+      <form className={css.form} action={handleSubmit}>
+        <input
+          className={css.input}
+          name="searchField"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search movies"
+        />
+        <button className={css.button} type="submit">
+          <IoSearchSharp className={css.icon} />
+        </button>
+      </form>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    </>
+  );
+};
+
+export default SearchBar;
