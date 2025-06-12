3f888d0cb8f1d838a2a240b73373a41bc256a8cd
Vitalii Mieliet <vitalii.mieliet@gmail.com>
2025-06-12 08:23:29 +0300

    style: add styles for Navigation component with active link

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,15 +1,23 @@
+import clsx from "clsx";
 import { NavLink } from "react-router-dom";
+import css from "./Navigation.module.css";
 
 const Navigation = () => {
+  const getNavLinkClass = ({ isActive }) =>
+    clsx(css.link, isActive && css.active);
   return (
-    <header>
+    <header className={css.header}>
       <nav>
         <ul>
           <li>
-            <NavLink to={"/"}>Home</NavLink>
+            <NavLink to={"/"} className={getNavLinkClass}>
+              Home
+            </NavLink>
           </li>
           <li>
-            <NavLink to={"/movies"}>Movies</NavLink>
+            <NavLink to={"/movies"} className={getNavLinkClass}>
+              Movies
+            </NavLink>
           </li>
         </ul>
       </nav>
